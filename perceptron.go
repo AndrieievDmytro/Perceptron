e3dd8aea3eccd0654ebb06de19aaa010b2e5f986
DmytroAndrieiev <63021874+DmytroAndrieiev@users.noreply.github.com>
2021-03-29 13:06:37 +0200

    Perceptron aded

--- perceptron.go
+++ perceptron.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 var (
-	// trainFile     string = "data\\train.txt" //data file
-	// testFile      string = "data\\test.txt"  //data file
-	weightsString string  = "[1,3,6]"
-	threshold     float64 = 67
-	weightsNum    []float64
-	alfa          float64 = 0.1
-	output        int
-	// beta          float64
-	// dimension     int
+	trainFile       string = "data\\iris_training.txt" //data file
+	testFile        string = "data\\iris_test.txt"     //data file
+	weightsString   string
+	threshold       float64
+	weightsNum      []float64
+	alfa            float64
+	dimension       int
+	desireAccurancy float64
 )
 
 // Activation function
@@ -24,63 +28,168 @@ var (
 type Object struct {
 	Params []float64
 	Name   string
-	// lable  int
+	Lable  int
+	Output int
 }
 type Objects struct {
-	Ob []Object
+	Obj []Object
 }
 
 func init() {
-	// flag.StringVar(&trainFile, "tr", trainFile, "train-file")
-	// flag.StringVar(&testFile, "ts", testFile, "test-file")
-	// flag.IntVar(&dimension, "d", dimension, "dimension")
+	flag.StringVar(&trainFile, "tr", trainFile, "train-file")
+	flag.StringVar(&testFile, "ts", testFile, "test-file")
+	flag.IntVar(&dimension, "d", dimension, "dimension")
+	flag.Float64Var(&desireAccurancy, "acc", desireAccurancy, "Desire accurancy")
 	flag.StringVar(&weightsString, "w", weightsString, "weights in form [val1,val2,valN]")
 	flag.Float64Var(&threshold, "t", threshold, "threshold")
 	flag.Float64Var(&alfa, "a", alfa, "alfa")
 }
 
-func stringToFloatArray(value string) []float64 {
+func readCsv(path string) ([][]string, error) {
+	dataFile, err := os.OpenFile(path, os.O_RDONLY, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer dataFile.Close()
+	if err == nil {
+		// Reading text from file
+		buf, rerr := ioutil.ReadAll(dataFile)
+		if rerr != nil {
+			fmt.Println("Read CSV error: " + rerr.Error())
+		}
+
+		// Parsing from comma-separated text
+		r := csv.NewReader(strings.NewReader(string(buf)))
+		records, err := r.ReadAll()
+		if err != nil {
+			fmt.Println("Parse CSV error: " + err.Error())
+		}
+		return records, nil
+	}
+	return nil, err
+}
+
+func convertStrArrayToJson(records [][]string) string {
+	// Converting from array of string to JSON
+	jsonData := ""
+	strNum := 0
+	for _, record := range records {
+		wrongStr := false
+		if len(record) < dimension || len(record) > dimension+1 {
+			fmt.Println("Wrong parameters count")
+			wrongStr = true
+		}
+		var objName string
+		if len(record) == dimension+1 {
+			objName = record[len(record)-1] // Cutting Object name
+			record = record[:len(record)-1]
+		}
+		strNum++
+		stringArray := "["                // Opening sq bracket
+		for _, arrField := range record { // Filling string representation of array
+			_, err := strconv.ParseFloat(arrField, 64)
+			if err != nil {
+				fmt.Println("Wrong parameters type in string: ", strNum)
+				wrongStr = true
+			}
+			stringArray += arrField + ","
+		}
+		stringArray = stringArray[:len(stringArray)-1] // Removing last ',' character
+		stringArray += "]"                             // Closing sq bracket
+		if !wrongStr {
+			jsonData += "{ \"name\": \"" + objName + "\", \"params\":" + stringArray + ", \"Distance\": [] }," // Converting to JSON
+		}
+	}
+	jsonData = "[" + jsonData[:len(jsonData)-1] + "]"
+	return jsonData
+
+}
+
+func (obj *Objects) readData(path string) {
+	records, err := readCsv(path)
+	if err == nil {
+		jsonData := convertStrArrayToJson(records)
+		json.Unmarshal([]byte(jsonData), &obj.Obj)
+	} else {
+		fmt.Println("Read CSV error: " + err.Error())
+	}
+}
+
+func stringToFloatArray(value string) {
 	str := value
-	var numVal []float64
-	err := json.Unmarshal([]byte(str), &numVal)
+	err := json.Unmarshal([]byte(str), &weightsNum)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return numVal
 }
 
-func computeOutput(input []float64) int {
+func (obj *Object) computeOutput() {
 	var dotProduct float64
-	for i := range input {
-		dotProduct += input[i] * weightsNum[i]
+	for i := range obj.Params {
+		dotProduct += obj.Params[i] * weightsNum[i]
 	}
-	fmt.Println("Dot product:", dotProduct)
+	// fmt.Println("Dot product:", dotProduct)
 	if dotProduct >= threshold {
-		output = 1
-	} else if dotProduct < threshold {
-		output = 0
+		obj.Output = 1
+	} else {
+		obj.Output = 0
 	}
-	return output
 }
 
-func train(input []float64, desireVal int) {
-	err := desireVal - output
+func (obj *Object) train() {
+	err := obj.Lable - obj.Output
 	for i := range weightsNum {
-		weightsNum[i] += float64(err) * alfa * input[i]
+		weightsNum[i] += float64(err) * alfa * obj.Params[i]
 	}
-	fmt.Println("Weight vector: ", weightsNum)
+	threshold += float64(err) * alfa * -1
+
+	// fmt.Println("Weight vector: ", weightsNum, "threashold: ", threshold)
+}
+
+func (objects *Objects) assignLabels(name string) {
+	for i, obj := range objects.Obj {
+		if name == obj.Name {
+			objects.Obj[i].Lable = 0
+		} else {
+			objects.Obj[i].Lable = 1
+		}
+	}
+}
+
+func getResult() {
+	tr := new(Objects)
+	tr.readData(trainFile)
+	ts := new(Objects)
+	ts.readData(testFile)
+	chkName := tr.Obj[0].Name
+	tr.assignLabels(chkName)
+	ts.assignLabels(chkName)
+	accuracy := 0.0
+	for iter := 0; accuracy < desireAccurancy && iter < 1000000; iter++ {
+		counter := len(ts.Obj)
+		// Train perceptron
+		for _, obj_train := range tr.Obj {
+			obj_train.computeOutput()
+			if obj_train.Lable != obj_train.Output {
+				obj_train.train()
+			}
+		}
+		// fmt.Println("Weight vector: ", weightsNum, "threashold: ", threshold)
+		// Calculate accurancy in test data
+		for _, obj_test := range ts.Obj {
+			obj_test.computeOutput()
+			if obj_test.Output != obj_test.Lable {
+				counter--
+			}
+		}
+		accuracy = (float64(counter) / float64(len(ts.Obj))) * 100.0
+		fmt.Println("Accurancy [", iter, "]", accuracy, "%")
+	}
 }
 
 func main() {
 	flag.Parse()
-	weightsNum = stringToFloatArray(weightsString)
-	testInput := []float64{10, 5, 2}
-	for {
-		output = computeOutput(testInput)
-		if output != 1 {
-			train(testInput, 1)
-		} else {
-			break
-		}
-	}
+	stringToFloatArray(weightsString)
+	fmt.Println(weightsNum)
+	getResult()
 }
